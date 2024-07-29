4bb0872340257ff79cf0321ea51f2ac7c9cd6542
Andres Marafioti <andimarafioti@gmail.com>
2024-07-29 13:20:57 +0000

    upload_data finishing touches

--- create_only_with_pdfs/upload_data.py
+++ create_only_with_pdfs/upload_data.py
@@ -1,27 +1,29 @@
 from datasets import load_from_disk, concatenate_datasets
 from tqdm import tqdm
-import shutil
 import os
 
 def get_datasets():
+    if os.path.isdir('/fsx/m4/datasets/docmatix_pdf/concatenated'):
+       return load_from_disk('/fsx/m4/datasets/docmatix_pdf/concatenated') 
+    
     hf_datasets = []
     for shard_nr in tqdm(range(200)):
         try:
             hf_datasets.append(load_from_disk(f'/fsx/m4/datasets/docmatix_pdf/shard_{shard_nr}'))
         except Exception as e:
-            if os.path.isdir(f'/fsx/m4/datasets/docmatix_pdf/shard_{shard_nr}'):
-                shutil.rmtree(f'/fsx/m4/datasets/docmatix_pdf/shard_{shard_nr}')
+            # if os.path.isdir(f'/fsx/m4/datasets/docmatix_pdf/shard_{shard_nr}'):
+            #     shutil.rmtree(f'/fsx/m4/datasets/docmatix_pdf/shard_{shard_nr}')
             print(f"Error loading dataset from: {shard_nr}")
             print(e)
-    return hf_datasets
+    hf_data = concatenate_datasets(hf_datasets)
+    hf_data.save_to_disk('/fsx/m4/datasets/docmatix_pdf/concatenated')
+    return hf_data
 
 
 data = get_datasets()
 
-print(data)
-print(data[0].features)
-print(data[0]['texts'][0])
-print(data[0]['pdf'][0][:10])
-
-hf_data = concatenate_datasets(data)
-hf_data.push_to_hub('HuggingFaceM4/Docmatix', 'pdf')
+print(data.features)
+print(data[0]['texts'])
+print(data[0]['pdf'][:10])
+print(len(data))
+data.push_to_hub('HuggingFaceM4/Docmatix', 'pdf')
